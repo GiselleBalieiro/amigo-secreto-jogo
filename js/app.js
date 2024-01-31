29bb8cbf346d39028677ac3991b58ce7896097f7
Giselle Balieiro <153029843+GiselleBalieiro@users.noreply.github.com>
2024-01-31 18:46:04 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,57 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById ('nome-amigo');
+    if (amigo.value == '') {
+        alert ('Informe um nome.')
+        return; 
+    }
+    if (amigos.includes(amigo.value)) {
+        alert ('Esse nome já foi adicionado.')
+        return; 
+    }
+    let lista = document.getElementById ('lista-amigos');
+    amigos.push(amigo.value);
+    
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    }
+        else {
+            lista.textContent = `${lista.textContent}, ${amigo.value}`;
+        }
+        amigo.value = '';
+}
+
+function sortear() {
+    if (amigos.length < 4) {
+        alert ('É necessário adicionar, no mínimo, quatro nomes.');
+        return;
+    }
+    embaralha(amigos);
+    let sorteio = document.getElementById ('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+    
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>'
+        }
+            else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>'
+        }
+    }
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById ('lista-amigos').innerHTML = '';
+    document.getElementById ('lista-sorteio').innerHTML = '';
+}
